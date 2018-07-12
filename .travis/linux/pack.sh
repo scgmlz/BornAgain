595798a3592875a0573178fad822eb0f68a4d453
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-07-12 11:47:25 +0200

    trying to fix the automatic upload of deb package

--- .travis/linux/pack.sh
+++ .travis/linux/pack.sh
@@ -1,3 +1,3 @@
 docker exec dd bash -c "cd build; cpack"
-docker exec dd bash -c "cd build; mv *.deb  bornagain-dev-xenial.deb"
-docker exec dd bash -c "cp *.deb  ../"
+docker exec dd bash -c "cd build; mv *.deb  BornAgain-dev-xenial.deb"
+docker exec dd bash -c 'cp $(find ./ -iname "BornAgain*.deb" | head -1) ./'
