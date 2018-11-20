0ebc8f9e7a76a4ca3475115abd75c8ac9bd05b41
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-11-20 11:43:10 +0100

    Separate definitions and implementations of classes inside CsvNamespace.h

--- GUI/coregui/Views/ImportDataWidgets/CsvImportAssistant/CsvNamespace.h
+++ GUI/coregui/Views/ImportDataWidgets/CsvImportAssistant/CsvNamespace.h
@@ -1,3 +1,18 @@
+// ************************************************************************** //
+//
+//  BornAgain: simulate and fit scattering at grazing incidence
+//
+//! @file      GUI/coregui/Views/ImportDataWidgets/CsvImportAssistant/CsvNamespace.h
+//! @brief     Defines namespace csv
+//!
+//! @homepage  http://www.bornagainproject.org
+//! @license   GNU General Public License v3 or higher (see COPYING)
+//! @copyright Forschungszentrum Jülich GmbH 2018
+//! @authors   Scientific Computing Group at MLZ (see CITATION, AUTHORS)
+//
+// ************************************************************************** //
+
+
 #ifndef CSVNAMESPACE_H
 #define CSVNAMESPACE_H
 
