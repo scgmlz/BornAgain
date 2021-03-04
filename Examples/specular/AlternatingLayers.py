aa52f72b58f3861fd562a36cde90369381870fea
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2021-03-04 16:08:03 +0100

    <72 chars/line

--- Examples/specular/AlternatingLayers.py
+++ Examples/specular/AlternatingLayers.py
@@ -9,10 +9,10 @@ from bornagain import deg, angstrom
 
 def get_sample():
     # Define materials
-    m_ambient = ba.MaterialBySLD("Ambient", 0.0, 0.0)
-    m_ti = ba.MaterialBySLD("Ti", -1.9493e-06, 0.0)
-    m_ni = ba.MaterialBySLD("Ni", 9.4245e-06, 0.0)
-    m_substrate = ba.MaterialBySLD("SiSubstrate", 2.0704e-06, 0.0)
+    m_ambient = ba.MaterialBySLD("Vacuum", 0, 0)
+    m_ti = ba.MaterialBySLD("Ti", -1.9493e-06, 0)
+    m_ni = ba.MaterialBySLD("Ni", 9.4245e-06, 0)
+    m_substrate = ba.MaterialBySLD("SiSubstrate", 2.0704e-06, 0)
 
     # Define layers
     ambient_layer = ba.Layer(m_ambient)
@@ -33,7 +33,7 @@ def get_sample():
 
 def get_simulation(sample, scan_size=500):
     simulation = ba.SpecularSimulation()
-    scan = ba.AngularSpecScan(1.54*angstrom, scan_size, 0.0*deg, 2.0*deg)
+    scan = ba.AngularSpecScan(1.54*angstrom, scan_size, 0*deg, 2*deg)
     simulation.setScan(scan)
     simulation.setSample(sample)
     return simulation
