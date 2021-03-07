5bb3ea2a74c221e5f7727aad8f251e799c63deff
Joachim Wuttke (h) <j.wuttke@fz-juelich.de>
2021-03-07 16:59:35 +0100

    copy edit BiMaterialCylinders example

--- Examples/scatter2d/BiMaterialCylinders.py
+++ Examples/scatter2d/BiMaterialCylinders.py
@@ -19,29 +19,26 @@ def get_sample():
     material_Substrate = ba.HomogeneousMaterial("Substrate", 3.212e-06,
                                                 3.244e-08)
     material_Teflon = ba.HomogeneousMaterial("Teflon", 2.9e-06, 6.019e-09)
-    material_Vacuum = ba.HomogeneousMaterial("Vacuum", 0.0, 0.0)
+    material_Vacuum = ba.HomogeneousMaterial("Vacuum", 0, 0)
 
     # Define form factors
-    ff_1 = ba.FormFactorCylinder(10.0*nm, 4.0*nm)
-    ff_2 = ba.FormFactorCylinder(10.0*nm, 10.0*nm)
+    ff_1 = ba.FormFactorCylinder(10*nm, 4*nm)
+    ff_2 = ba.FormFactorCylinder(10*nm, 10*nm)
 
     # Define particles
-    particle_1 = ba.Particle(material_Ag, ff_1)
-    particle_1_position = kvector_t(0.0*nm, 0.0*nm, 10.0*nm)
-    particle_1.setPosition(particle_1_position)
-    particle_2 = ba.Particle(material_Teflon, ff_2)
+    subparticle_1 = ba.Particle(material_Ag, ff_1)
+    subparticle_1.setPosition(kvector_t(0, 0, 10*nm))
+    subparticle_2 = ba.Particle(material_Teflon, ff_2)
 
     # Define composition of particles at specific positions
-    particle_3 = ba.ParticleComposition()
-    particle_3.addParticle(particle_1)
-    particle_3.addParticle(particle_2)
-    particle_3_position = kvector_t(0.0*nm, 0.0*nm, -10.0*nm)
-    particle_3.setPosition(particle_3_position)
+    particle = ba.ParticleComposition()
+    particle.addParticle(subparticle_1)
+    particle.addParticle(subparticle_2)
+    particle.setPosition(kvector_t(0, 0, -10*nm))
 
     # Define particle layouts
     layout = ba.ParticleLayout()
-    layout.addParticle(particle_3, 1.0)
-    layout.setWeight(1)
+    layout.addParticle(particle)
     layout.setTotalParticleSurfaceDensity(1)
 
     # Define layers
@@ -58,7 +55,7 @@ def get_sample():
 
 
 def get_simulation(sample):
-    beam = ba.Beam(100000000.0, 0.1*nm, ba.Direction(0.2*deg, 0*deg))
+    beam = ba.Beam(1e9, 0.1*nm, ba.Direction(0.2*deg, 0*deg))
     detector = ba.SphericalDetector(100, 2*deg, 0*deg, 1*deg)
     simulation = ba.GISASSimulation(beam, sample, detector)
     return simulation
